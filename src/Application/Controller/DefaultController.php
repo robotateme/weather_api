8b83e82c05357ddb997e9f4203abfff5b208183d
robotateme <jarwork884@gmail.com>
2025-04-07 15:57:51 +0300

    Initial

---
+++ src/Application/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Controller;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class DefaultController
+{
+    #[Route('/', name: 'home')]
+    public function indexAction(): JsonResponse
+    {
+        return new JsonResponse(['Hello World!']);
+    }
+}
